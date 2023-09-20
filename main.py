d1089692c5a1432ebabec65cd18258b647f64fef
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-09-20 07:08:13 +0100

    refactored

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-with open("./books/frankenstein.txt") as file:
-    file_contents = file.read()
+def get_book_text(path):
+    with open(path) as file:
+        return file.read()
 
-    def count_words(text):
-        words = text.split()
-        return len(words)
 
-    print(count_words(file_contents))
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def main():
+    path = "./books/frankenstein.txt"
+    file_contents = get_book_text(path)
+    total_num_words = count_words(file_contents)
+    print(total_num_words)
+
+
+main()
