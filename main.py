27aac9290b6014f7d9123f313606679d7c2a6b20
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-02-10 12:56:24 +0000

    bookbot character report

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-def get_character_count(text: str) -> dict[str, int]:
-    character_count = {}
+def get_char_count(text: str) -> dict[str, int]:
+    char_count = {}
     lowercase_text = text.lower()
 
-    for character in lowercase_text:
-        if character not in character_count:
-            character_count[character] = 0
-        character_count[character] += 1
+    for char in lowercase_text:
+        if char not in char_count:
+            char_count[char] = 0
+        char_count[char] += 1
 
-    return character_count
+    return char_count
 
 
 def get_num_words(text: str) -> int:
@@ -20,10 +20,44 @@ def get_book_text() -> str:
         return f.read()
 
 
+def convert_to_list_of_dicts(char_count: dict[str, int]) -> list[dict[str, int]]:
+    result = []
+
+    for char, count in char_count.items():
+        char_dict = {"char": char, "count": count}
+        result.append(char_dict)
+
+    return result
+
+
+def sort_on(char_count):
+    return char_count["count"]
+
+
+def sort_char_count(list_of_char_count):
+    return list_of_char_count.sort(reverse=True, key=sort_on)
+
+def print_char_stat(sorted_chars):
+    for char_count in sorted_chars:
+        char = char_count["char"]
+        if char.isalpha():
+            count = char_count["count"]
+            print(f"The '{char}' char was found {count} times")
+
+
+def print_report(text: str, list_of_char_count: list[dict[str, int]]):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{get_num_words(text)} words found in the document")
+    print_char_stat(list_of_char_count)
+    print("--- End report ---")
+
+
 def main():
     text = get_book_text()
-    num_words = get_num_words(text)
-    character_count = get_character_count(text)
+    char_count = get_char_count(text)
+    list_of_char_count = convert_to_list_of_dicts(char_count)
+    sort_char_count(list_of_char_count)
+    print_report(text, list_of_char_count)
 
 
 main()
