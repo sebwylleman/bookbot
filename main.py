346e2a262c99de266bc0e3276f68b4462644c8ee
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-02-10 10:18:25 +0000

    printing the count of each non whitespace character from the book

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-def count_words(file_contents: str) -> int:
-    words = file_contents.split()
+def get_character_count(words: list[str]) -> dict[str, int]:
+    character_count = {}
+
+    for word in words:
+        for char in word.lower():
+            if char not in character_count:
+                character_count[char] = 0
+            character_count[char] += 1
+
+    return character_count
+
+
+def get_num_words(words: list[str]) -> int:
     return len(words)
 
 
-def main():
+def get_book_text() -> str:
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(count_words(file_contents))
+        return f.read()
+
+
+def main():
+    file_contents = get_book_text()
+
+    words = file_contents.split()
+    get_num_words(words)
+    print(get_character_count(words))
 
 
 main()
