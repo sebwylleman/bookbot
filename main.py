1b03d4b8e6a8a5ee06fdb33460261015d178022f
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-03-04 21:02:55 +0200

    refactor

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import sys
 
 from stats import (
-    convert_to_list_of_dicts,
-    get_book_text,
-    get_char_count,
-    print_report,
-    sort_char_count,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
 )
 
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    char_count = get_char_count(text)
-    list_of_char_count = convert_to_list_of_dicts(char_count)
-    sort_char_count(list_of_char_count)
-    print_report(book_path, text, list_of_char_count)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,53 +1,26 @@
-def get_char_count(text: str) -> dict[str, int]:
-    char_count = {}
-    lowercase_text = text.lower()
-
-    for char in lowercase_text:
-        if char not in char_count:
-            char_count[char] = 0
-        char_count[char] += 1
-
-    return char_count
-
-
-def get_num_words(text: str) -> int:
+def get_num_words(text):
     words = text.split()
     return len(words)
 
 
-def get_book_text(file_path) -> str:
-    with open(file_path) as f:
-        return f.read()
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
-def convert_to_list_of_dicts(char_count: dict[str, int]) -> list[dict[str, int]]:
-    result = []
-
-    for char, count in char_count.items():
-        char_dict = {"char": char, "count": count}
-        result.append(char_dict)
-
-    return result
+def sort_on(d):
+    return d["num"]
 
 
-def sort_on(char_count: dict[str, int]) -> int:
-    return char_count["count"]
-
-
-def sort_char_count(list_of_char_count: list[dict[str, int]]):
-    return list_of_char_count.sort(reverse=True, key=sort_on)
-
-
-def print_char_stat(sorted_chars: list[dict[str, int]]) -> str:
-    for char_count in sorted_chars:
-        char = char_count["char"]
-        if char.isalpha():
-            count = char_count["count"]
-            print(f"The '{char}' char was found {count} times")
-
-
-def print_report(file_path: str, text: str, list_of_char_count: list[dict[str, int]]):
-    print("--- Begin report of {file_path} ---")
-    print(f"{get_num_words(text)} words found in the document")
-    print_char_stat(list_of_char_count)
-    print("--- End report ---")
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
